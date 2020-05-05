d88467269a3cefdb74cce55e1cebda79ee2e1191
Arturas Solncevas <arturassolncevas@gmail.com>
2020-05-05 22:45:46 +0200

    Modify login form

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/Home'
+import LoginPage from './pages/Login'
 import 'antd/dist/antd.css'
 import '../../styles/less/app.less'
 
 export default function App() {
   return (
     <Switch>
-      <Route exact patth="/" component={HomePage} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/login" component={LoginPage} />
     </Switch>
   )
 }
--- resources/js/components/pages/Login.js
+++ resources/js/components/pages/Login.js
@@ -1,74 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Form, Icon, Input, 
-         Button, Checkbox }  from 'antd';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Formik } from 'formik'
+import { Form, Input, Button, Checkbox, Row, Col, Card } from 'antd';
 
-const FormItem = Form.Item;
-class NormalLoginForm extends React.Component {
 
-  checkUsername = (rule, value, callback) => {
-    const form = this.props.form;
-    form.setFields({
-      username: {
-        value: 'asdas'
-      }
-    });
-    // form.setFieldsValue ('pedro, manada');
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [
-              {required: true, message: 'Please input your username!'},
-              {validator: this.checkUsername}
-            ],
-          })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} 
-             placeholder="Username" />
-            )}
-        </FormItem>
-
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} 
-             type="password" placeholder="Password" />
-            )}
-        </FormItem>
-        
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-            )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </FormItem>
-      </Form>
-    );
-  }
+const layout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 },
 }
 
-const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+export default class LoginForm extends Component {
 
-ReactDOM.render(<WrappedNormalLoginForm />, document.getElementById('app'));
+  constructor(props) {
+    super(props)
+    this.state = { formDefault: { email: 'email', password: 'password' }, errors: { name: { message: null, status: "success" } } }
+    this.formRef = React.createRef();
+  }
+
+  handleFormSubmit(values) {
+    let newState = { ...this.state }
+    newState.errors.name = { status: "error", message: "Wrong username" }
+    this.setState({ ...this.state, })
+  }
+
+  handleFormSubmitFailed(errorInfo) {
+    console.log('Failed:', errorInfo);
+  };
+
+  render() {
+    return (
+      <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
+        <Col lg={6} >
+          <Card title="Login">
+            <Form
+              ref={this.formRef}
+              {...layout}
+              name="basic"
+              initialValues={this.state.formDefault}
+              onFinish={(e) => { this.handleFormSubmit(e) }}
+              onFinishFailed={this.handleFormSubmitFailed}
+            >
+              <Form.Item
+                label="Email"
+                name="email"
+                validateStatus={this.state.errors.name.status}
+                help={this.state.errors.name.message}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item name="remember" valuePropName="checked">
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item
+              >
+                <Row justify="end">
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                </Row>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    )
+  }
+}
