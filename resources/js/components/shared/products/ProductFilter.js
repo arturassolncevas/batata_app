2aac38a03bc9e0d97786aab0eeaad7134158cdb3
Arturas Solncevas <arturassolncevas@gmail.com>
2020-09-19 12:32:33 +0200

    Move product filter values in form

--- resources/js/components/shared/products/ProductFilter.js
+++ resources/js/components/shared/products/ProductFilter.js
@@ -4,6 +4,7 @@ import { SearchOutlined, CaretDownOutlined, CarerUpOutlined, CaretUpOutlined, Ar
 import { injectIntl } from 'react-intl'
 import { formatNumber } from '../../shared/helpers/priceFormatter'
 import qs from 'query-string';
+import { FormContext } from 'antd/lib/form/context';
 
 function filter(inputValue, path) {
   return path.some(option => option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1);
@@ -18,7 +19,17 @@ class ProductFilter extends Component {
   constructor(props) {
     super(props)
     this.formRef = React.createRef();
-    this.state = { advancedSearchOpened: true, category: {} }
+    this.state = { advancedSearchOpened: true }
+
+  }
+
+  componentDidMount() {
+    this.setQueryValues()
+  }
+
+  setQueryValues() {
+    let queryObject = qs.parse(this.props.history.location.search)
+    this.formRef.current.setFieldsValue(queryObject)
   }
 
   handleAdvancedSearchClick() {
@@ -30,33 +41,49 @@ class ProductFilter extends Component {
     this.setState({ ...this.state, category: category || {} })
   }
 
-
   async handleSearch() {
+    let formValues = this.formRef.current.getFieldsValue()
     let data = {
-      category_id: this.state.category.id,
-      ...this.formRef.current.getFieldsValue(),
+      ...formValues,
     }
     this.props.history.push(`/products?${qs.stringify(data)}`)
   }
 
   render() {
     return (
-      <Row style={{ flexDirection: "column" }}>
+
+      <Form
+        ref={this.formRef}
+        initialValues={this.state.forma}
+        onFinish={(e) => { }}
+        {...layout}
+      >
+
         <Row >
           <Col xl={8} >
-            <Cascader
-              style={{ width: "100%" }}
-              options={this.props.categories}
-              onChange={(val, options) => { this.handleCategoryChange(val, options) }}
-              placeholder="Search product"
-              showSearch={{ filter }}
-              allowClear
-              changeOnSelect
+            <Form.Item
+              name="category_id"
+              valuePropName="value"
+              style={{marginBottom: '0px'}}
             >
-            </Cascader>
+              <Cascader
+                style={{ width: "100%" }}
+                options={this.props.categories}
+                onChange={(val, options) => { this.handleCategoryChange(val, options) }}
+                placeholder="Search product"
+                showSearch={{ filter }}
+                allowClear
+                changeOnSelect
+              >
+              </Cascader>
+            </Form.Item>
           </Col>
           <Col>
-            <Button type="primary" icon={<SearchOutlined />} style={{ padding: "4px 15px", width: "auto" }} onClick={() => { this.handleSearch() }}/>
+            <Form.Item
+              style={{marginBottom: '0px'}}
+            >
+              <Button type="primary" icon={<SearchOutlined />} style={{ padding: "4px 15px", width: "auto" }} onClick={() => { this.handleSearch() }} />
+            </Form.Item>
           </Col>
           <Col
             className="primary-color"
@@ -72,48 +99,41 @@ class ProductFilter extends Component {
             {this.state.advancedSearchOpened ? <CaretUpOutlined /> : <CaretDownOutlined />}
           </Col>
         </Row>
-        <Row style={{ display: !this.state.advancedSearchOpened && "none" }}>
-          <Form
-            ref={this.formRef}
-            initialValues={{}}
-            onFinish={(e) => {}}
-            {...layout}
-          >
 
-            <Form.Item
-              label={this.props.intl.formatMessage({ id: 'searchFilter.price.name' })}
-              name="price_to"
-            >
-              <Input.Group compact>
-                <Form.Item
-                  name="price_from"
-                >
-                  <InputNumber
-                    placeholder={this.props.intl.formatMessage({ id: 'searchFilter.price.min' })}
-                    formatter={value => formatNumber.format(value)}
-                    parser={value => formatNumber.parse(value)}
-                    min={0}
-                  />
-                </Form.Item>
+        <Row style={{ display: !this.state.advancedSearchOpened && "none" }}>
+          <Form.Item
+            label={this.props.intl.formatMessage({ id: 'searchFilter.price.name' })}
+            name="price_to"
+          >
+            <Input.Group compact>
+              <Form.Item
+                name="price_from"
+              >
+                <InputNumber
+                  placeholder={this.props.intl.formatMessage({ id: 'searchFilter.price.min' })}
+                  formatter={value => formatNumber.format(value)}
+                  parser={value => formatNumber.parse(value)}
+                  min={0}
+                />
+              </Form.Item>
               &nbsp;
               -
               &nbsp;
               <Form.Item
-                  name="price_to"
-                >
-                  <InputNumber
-                    placeholder={this.props.intl.formatMessage({ id: 'searchFilter.price.max' })}
-                    formatter={value => formatNumber.format(value)}
-                    parser={value => formatNumber.parse(value)}
-                    min={0}
-                  />
-                </Form.Item>
+                name="price_to"
+              >
+                <InputNumber
+                  placeholder={this.props.intl.formatMessage({ id: 'searchFilter.price.max' })}
+                  formatter={value => formatNumber.format(value)}
+                  parser={value => formatNumber.parse(value)}
+                  min={0}
+                />
+              </Form.Item>
 
-              </Input.Group>
-            </Form.Item>
-          </Form>
+            </Input.Group>
+          </Form.Item>
         </Row>
-      </Row>
+      </Form>
     )
   }
 }
